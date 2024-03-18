d88a8222ff98bfa9ac6220ca2f7fc0ada898fc0b
Jay Faust <jay.faust3@gmail.com>
2024-03-18 13:42:41 -0400

    Made cache expiration configuarble

--- user.api/Application/Services/Cache/MemoryCacheService.cs
+++ user.api/Application/Services/Cache/MemoryCacheService.cs
@@ -4,28 +4,44 @@ namespace Application.Services.Cache;
 
 public class MemoryCacheService : ICacheService
 {
+    private readonly double _slidingExpirationMinutes;
+    private readonly double _absoluteExpirationRelativeToNowMinutes;
     private readonly IMemoryCache _memoryCache;
-    private readonly MemoryCacheEntryOptions _cacheSetOptions;
 
     public MemoryCacheService(IMemoryCache memoryCache)
     {
-        _memoryCache = memoryCache;
-        _cacheSetOptions = new MemoryCacheEntryOptions
-        {
-            Size = 1024,
-            SlidingExpiration = TimeSpan.FromMinutes(30),
-            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(180)
-        };
+        _slidingExpirationMinutes = double.Parse
+        (
+            Environment.GetEnvironmentVariable("CACHE_SLIDING_EXPIRATION_MINUTES") ?? "30"
+        );
 
+        _absoluteExpirationRelativeToNowMinutes = double.Parse
+        (
+            Environment.GetEnvironmentVariable("ABSOLUTE_EXPIRATION_RELATIVE_TO_NOW_MINUTES") ?? "60"
+        );
+
+        _memoryCache = memoryCache;
     }
 
     public Task<TItem?> GetItemAsync<TItem>(string key) =>
         Task.Run(() => _memoryCache.Get<TItem>(key));
 
     public Task SetItemAsync<TItem>(string key, TItem item) =>
-        Task.Run(() => _memoryCache.Set(key, item, _cacheSetOptions));
+        Task.Run
+        (
+            () =>
+                _memoryCache.Set
+                (
+                    key,
+                    item,
+                    new MemoryCacheEntryOptions
+                    {
+                        SlidingExpiration = TimeSpan.FromMinutes(_slidingExpirationMinutes),
+                        AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(_absoluteExpirationRelativeToNowMinutes)
+                    }
+                )
+        );
 
     public Task RemoveItemAsync<TItem>(string key) =>
         Task.Run(() => _memoryCache.Remove(key));
 }
-
