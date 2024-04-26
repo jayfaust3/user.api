5c47bd449e592c6490a841a19dac1b6a6c0ba880
Jay Faust <jay.faust3@gmail.com>
2024-04-25 21:01:55 -0400

    WIP: Added Docker script

---
+++ scripts/publish-to-docker-registry.ts
@@ -0,0 +1,29 @@
+import { config } from 'dotenv';
+import { exec } from 'child_process';
+import { SSM } from 'aws-sdk';
+import { v4 as uuid } from 'uuid';
+
+type PutParams = SSM.PutParameterRequest;
+type PutResult = SSM.PutParameterResult;
+
+const { DOCKER_USERNAME, DOCKER_PASSWORD } = process.env;
+
+if (!DOCKER_USERNAME || !DOCKER_PASSWORD) throw new Error(`Environment is missing Docker credentials`);
+
+config();
+
+const buildId = uuid();
+
+const command = ``;
+
+exec("ls -la", (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
