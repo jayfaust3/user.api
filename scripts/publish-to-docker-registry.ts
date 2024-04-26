2b04f72d083fe7a99775e1f46eb2cacf17a74327
Jay Faust <jay.faust3@gmail.com>
2024-04-25 22:11:52 -0400

    WIP: Updated Docker script

--- scripts/publish-to-docker-registry.ts
+++ scripts/publish-to-docker-registry.ts
@@ -6,15 +6,37 @@ import { v4 as uuid } from 'uuid';
 type PutParams = SSM.PutParameterRequest;
 type PutResult = SSM.PutParameterResult;
 
-const { DOCKER_USERNAME, DOCKER_PASSWORD } = process.env;
+const { DOCKER_REPOSITORY, DOCKER_USERNAME, DOCKER_PASSWORD } = process.env;
 
-if (!DOCKER_USERNAME || !DOCKER_PASSWORD) throw new Error(`Environment is missing Docker credentials`);
+if (
+    !DOCKER_REPOSITORY ||
+    !DOCKER_USERNAME ||
+    !DOCKER_PASSWORD
+) throw new Error(`Environment is missing Docker credentials`);
 
 config();
 
-const buildId = uuid();
+const pathToDockerfile = './user.api/Dockerfile';
 
-const command = ``;
+const imageName = 'user-service';
+
+const buildTag = uuid();
+
+const localImage = `${imageName}:${buildTag}`;
+
+const dockerImageURI = `${DOCKER_REPOSITORY}:${buildTag}`;
+
+const command = `
+docker build -t ${localImage} -f ${pathToDockerfile}
+
+docker tag ${localImage} ${dockerImageURI}
+
+echo ${DOCKER_PASSWORD} | docker login --username ${DOCKER_USERNAME} --password-stdin ${DOCKER_REPOSITORY}
+
+docker push ${dockerImageURI}
+
+docker logout ${DOCKER_REPOSITORY}
+`;
 
 exec(command, (error, stdout, stderr) => {
     if (error) {
