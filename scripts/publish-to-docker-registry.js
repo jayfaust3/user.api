f85282a0d6c7306b85551b4140215730c3a899eb
Jay Faust <jay.faust3@gmail.com>
2024-05-13 21:05:35 -0400

    Updated publish script to use aws cli

--- scripts/publish-to-docker-registry.js
+++ scripts/publish-to-docker-registry.js
@@ -8,7 +8,8 @@ const envFilePath = resolve(__dirname, '../.env');
 config({ path: envFilePath });
 
 const {
-  AWS_ENDPOINT,
+  // // AWS_ENDPOINT,
+  AWS_PROFILE,
   DOCKER_REPOSITORY,
   DOCKER_REGISTRY_IMAGE_URI_SSM_PARAM_NAME,
   DOCKER_USERNAME,
@@ -30,45 +31,77 @@ const buildTag = Date.now();
 
 const dockerImageURI = `${DOCKER_REPOSITORY}:${buildTag}`;
 
-const ssm = new AWS.SSM({ endpoint: AWS_ENDPOINT });
+// const ssm = new AWS.SSM({ endpoint: AWS_ENDPOINT });
 
-const putParameterParams = {
-  Name: DOCKER_REGISTRY_IMAGE_URI_SSM_PARAM_NAME,
-  Value: dockerImageURI,
-  Type: 'String',
-  Overwrite: true,
-};
+// const putParameterParams = {
+//   Name: DOCKER_REGISTRY_IMAGE_URI_SSM_PARAM_NAME,
+//   Value: dockerImageURI,
+//   Type: 'String',
+//   Overwrite: true,
+// };
 
-ssm.putParameter(putParameterParams, (err, data) => {
-  if (err) {
-    console.error('Error writing parameter:', err);
-  } else {
-    const pathToDockerfile = resolve(__dirname, '../user.api/Dockerfile');
+// ssm.putParameter(putParameterParams, (err, data) => {
+//   if (err) {
+//     console.error('Error writing parameter:', err);
+//   } else {
+//     const pathToDockerfile = resolve(__dirname, '../user.api/Dockerfile');
 
-    const publishCommand = `
-    echo ${DOCKER_PASSWORD} | docker login --username ${DOCKER_USERNAME} --password-stdin &&
+//     const publishCommand = `
+//     echo ${DOCKER_PASSWORD} | docker login --username ${DOCKER_USERNAME} --password-stdin &&
 
-    docker build -t ${imageName} -f ${pathToDockerfile} . &&
+//     docker build -t ${imageName} -f ${pathToDockerfile} . &&
 
-    docker tag ${imageName} ${dockerImageURI} &&
+//     docker tag ${imageName} ${dockerImageURI} &&
 
-    docker push ${dockerImageURI} &&
+//     docker push ${dockerImageURI} &&
 
-    docker logout
-    `;
+//     docker logout &&
 
-    exec(publishCommand, (error, stdout, stderr) => {
-      if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-      }
+//     aws ssm put-parameter --profile ${AWS_PROFILE} --name ${DOCKER_REGISTRY_IMAGE_URI_SSM_PARAM_NAME} --value ${dockerImageURI} --type String --overwrite
+//     `;
+
+//     exec(publishCommand, (error, stdout, stderr) => {
+//       if (error) {
+//         console.log(`error: ${error.message}`);
+//         return;
+//       }
     
-      if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-      }
+//       if (stderr) {
+//         console.log(`stderr: ${stderr}`);
+//         return;
+//       }
     
-      console.log(`stdout: ${stdout}`);
-    });
+//       console.log(`stdout: ${stdout}`);
+//     });
+//   }
+// });
+
+const pathToDockerfile = resolve(__dirname, '../user.api/Dockerfile');
+
+const publishCommand = `
+echo ${DOCKER_PASSWORD} | docker login --username ${DOCKER_USERNAME} --password-stdin &&
+
+docker build -t ${imageName} -f ${pathToDockerfile} . &&
+
+docker tag ${imageName} ${dockerImageURI} &&
+
+docker push ${dockerImageURI} &&
+
+docker logout &&
+
+aws ssm put-parameter --profile ${AWS_PROFILE} --name ${DOCKER_REGISTRY_IMAGE_URI_SSM_PARAM_NAME} --value ${dockerImageURI} --type String --overwrite
+`;
+
+exec(publishCommand, (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
   }
+    
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+    
+  console.log(`stdout: ${stdout}`);
 });
